7d52960c3f49f65da202f778b80c8e55ac01b2f0
Rasam Dorri <rdorr002@ucr.edu>
2025-02-14 13:30:05 -0800

    Update homework.R
    new new update? I could not find the HW3 :((((

--- homework.R
+++ homework.R
@@ -9,90 +9,102 @@
 library(tidyverse)
 ds <- read_csv("data_raw/rolling_stone_500.csv")
   
-### Question 1 ---------- 
+### Question 1 ----------
+# 1. Check the type of Year with glimpse
+# 2. Convert Year to numeric, reassign to ds
+# 3. Verify conversion with typeof
+glimpse(ds)
+ds <- ds %>%
+  mutate(Year = as.numeric(Year))
+typeof(ds$Year)
 
-#Use glimpse to check the type of "Year". 
-#Then, convert it to a numeric, saving it back to 'ds'
-#Use typeof to check that your conversion succeeded
-
-#ANSWER
-
-
-### Question 2 ---------- 
-
-# Using a dplyr function,
-# change ds so that all of the variables are lowercase
-
-#ANSWER
+### Question 2 ----------
+# Make all variable names lowercase
+ds <- ds %>%
+  rename_with(tolower)
 
 ### Question 3 ----------
-
-# Use mutate to create a new variable in ds that has the decade of the year as a number
-# For example, 1971 would become 1970, 2001 would become 2000
-# Hint: read the documentation for ?floor
-
-#ANSWER
+# Create new variable 'decade'
+# e.g., 1971 -> 1970, 2001 -> 2000
+ds <- ds %>%
+  mutate(decade = floor(year / 10) * 10)
 
 ### Question 4 ----------
-
-# Sort the dataset by rank so that 1 is at the top
-
-#ANSWER
+# Sort dataset by rank so that 1 is at top
+ds <- ds %>%
+  arrange(rank)
 
 ### Question 5 ----------
-
-# Use filter and select to create a new tibble called 'top10'
-# That just has the artists and songs for the top 10 songs
-
-#ANSWER
-
+# Create tibble 'top10' with just artist, song for top 10
+top10 <- ds %>%
+  filter(rank <= 10) %>%
+  select(artist, song)
 
 ### Question 6 ----------
-
-# Use summarize to find the earliest, most recent, and average release year
-# of all songs on the full list. Save it to a new tibble called "ds_sum"
-
-#ANSWER
-
+# Summarize earliest, most_recent, average release year of all songs
+ds_sum <- ds %>%
+  summarize(
+    earliest    = min(year, na.rm = TRUE),
+    most_recent = max(year, na.rm = TRUE),
+    average     = mean(year, na.rm = TRUE)
+  )
+ds_sum
 
 ### Question 7 ----------
+# Filter for earliest, most recent, "average-ist" years (rounded)
+# One filter command only, then sort by year
+ds %>%
+  filter(
+    year == ds_sum$earliest |
+    year == ds_sum$most_recent |
+    # average might not be an integer, so rounding is typical
+    year == round(ds_sum$average)
+  ) %>%
+  arrange(year)
 
-# Use filter to find out the artists/song titles for the earliest, most 
-# recent, and average-ist years in the data set (the values obtained in Q6). 
-# Use one filter command only, and sort the responses by year
+### Question 8 ----------
+# Fix the error for the song "Brass in Pocket" (should be 1979)
+# Then recalculate decade, ds_sum, and re-run the filter
+ds <- ds %>%
+  mutate(
+    year = ifelse(song == "Brass in Pocket", 1979, year)
+  ) %>%
+  mutate(decade = floor(year / 10) * 10)
 
-#ANSWER
+# Recompute ds_sum
+ds_sum <- ds %>%
+  summarize(
+    earliest    = min(year, na.rm = TRUE),
+    most_recent = max(year, na.rm = TRUE),
+    average     = mean(year, na.rm = TRUE)
+  )
+ds_sum
 
+# Filter again for the updated earliest/most_recent/average
+ds %>%
+  filter(
+    year == ds_sum$earliest |
+    year == ds_sum$most_recent |
+    year == round(ds_sum$average)
+  ) %>%
+  arrange(year)
 
-### Question 8 ---------- 
+### Question 9 ----------
+# Group by decade, ignoring NA values for decade,
+# then find average rank & number of songs
+ds %>%
+  filter(!is.na(decade)) %>%
+  group_by(decade) %>%
+  summarize(
+    avg_rank = mean(rank, na.rm = TRUE),
+    n_songs  = n()
+  )
 
-# There's and error here. The oldest song "Brass in Pocket"
-# is from 1979! Use mutate and ifelse to fix the error, 
-# recalculate decade, and then
-# recalculate the responses from Questions 6-7 to
-# find the correct oldest, averag-ist, and most recent songs
-
-#ANSWER
-
-
-### Question 9 ---------
-
-# Use group_by and summarize to find the average rank and 
-# number of songs on the list by decade. To make things easier
-# filter out the NA values from decade before summarizing
-# You don't need to save the results anywhere
-# Use the pipe %>% to string the commands together
-
-#ANSWER
-
-
-### Question 10 --------
-
-# Look up the dplyr "count" function
-# Use it to count up the number of songs by decade
-# Then use slice_max() to pull the row with the most songs
-# Use the pipe %>% to string the commands together
-
-#ANSWER
+### Question 10 ----------
+# Use dplyr::count to count songs by decade,
+# then slice_max() to find the decade with the most songs
+ds %>%
+  count(decade) %>%
+  slice_max(n, n = 1)
 
   
